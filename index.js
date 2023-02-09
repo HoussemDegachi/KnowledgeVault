3ea98d2987a10263a400f4945f61eb25cef8b2a5
HoussemDegachi <113510665+HoussemDegachi@users.noreply.github.com>
2023-02-09 15:02:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const lastDay = new Date(2023 , 1 , 17)
 const today = new Date()
 const daysLeft = ~~((lastDay - today) / (1000*60*60*24))
+const hiddenEl = document.querySelectorAll(".hidden")
+const observer = new IntersectionObserver((enteries) => {
+  enteries.forEach((entery) => {
+    if(entery.isIntersecting){
+      entery.target.classList.add('show')
+    }else{
+      entery.target.classList.remove('show')
+    }
+  })
+})
+
+hiddenEl.forEach((el) => observer.observe(el))
 
 function setDaysLeft(){
-    let daysHtmlSrc = document.querySelector(".discount__deal")
-    daysHtmlSrc.innerHTML = "Up to 80% discount | Ends in "+ daysLeft +" Days"
+    let daysEl = document.querySelector(".discount__deal")
+    daysEl.innerHTML = "Up to 80% discount | Ends in "+ daysLeft +" Days"
 }
+
 
 setDaysLeft()
